2fc04a318137b119bc87b78d11eebf5aae2f6597
Oleg Ivanov <ivanov@megaputer.ru>
2018-09-19 17:13:21 +0300

    HARDCODED: Force https in production

--- app/helpers/phab_auth/application_helper.rb
+++ app/helpers/phab_auth/application_helper.rb
@@ -1,9 +1,11 @@
 module PhabAuth
   module ApplicationHelper
     def phab_login_link
+      red_url = request.base_url + '/' + PhabAuth::Engine.mounted_path
+      red_url.gsub!('http://', 'https://') if Rails.env.production?
       l_params = {
         client_id: PhabAuth.client_id,
-        redirect_uri: request.base_url + '/' + PhabAuth::Engine.mounted_path,
+        redirect_uri: red_url,
         response_type: 'code'
       }
       link_to_params = [
