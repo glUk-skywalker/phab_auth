9896abe86df1348d69ce9f7e4df606372c7e5e57
Oleg Ivanov <ivanov@megaputer.ru>
2018-09-19 17:58:15 +0300

    Some logging

--- app/controllers/phab_auth/auth_controller.rb
+++ app/controllers/phab_auth/auth_controller.rb
@@ -5,7 +5,7 @@ module PhabAuth
         require 'net/http'
         red_url = main_app.root_url + '/' + PhabAuth::Engine.mounted_path
         red_url.gsub!('http://', 'https://') if Rails.env.production?
-          param = {
+        param = {
           client_id: PhabAuth.client_id,
           client_secret: PhabAuth.client_secret,
           code: params[:code],
@@ -14,12 +14,17 @@ module PhabAuth
         }
         uri = URI(PhabAuth.oauthserver_url + '/oauthserver/token/?' + URI.encode_www_form(param))
         uri.path.gsub!(%r{\/+}, '/')
-        t = JSON.parse(Net::HTTP.get(uri))['access_token']
-        if t
-          uri = URI(PhabAuth.oauthserver_url + "/api/user.whoami?access_token=#{t}")
-          uri.path.gsub!(%r{\/+}, '/')
-          user_info = JSON.parse(Net::HTTP.get(uri))['result']
-        end
+
+        resp_parsed = JSON.parse(Net::HTTP.get(uri))
+        t = resp_parsed['access_token']
+        logger.fatal(resp_parsed.to_s) unless t
+
+        uri = URI(PhabAuth.oauthserver_url + "/api/user.whoami?access_token=#{t}")
+        uri.path.gsub!(%r{\/+}, '/')
+
+        resp_parsed = JSON.parse(Net::HTTP.get(uri))
+        user_info = resp_parsed['result']
+        logger.fatal(resp_parsed.to_s) unless user_info
       end
       redirect_to main_app.send(PhabAuth.create_session_path, result: { user_info: user_info })
     end
